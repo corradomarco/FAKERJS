93898f6d246e103922cde834f55bf1241f5657c3
corradomarco <corrado.marco@iisgalvanimi.edu.it>
2020-01-03 17:35:14 +0000

    w

---
+++ fakerjs/routes/users.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var faker =require('faker');
+let vett = new Array();
+const fs = require ('fs');
+const poeti = require('../vettPoeti');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('createe');
+});
+router.get('/profile',(req,res)=>{
+    const r=poeti.find(p => p.id == req.query.id);
+    
+    res.render('profile',{title:`About ${r.nome}`,r });
+
+});
+
+function createFakePerson()
+{
+ let randomName = faker.name.findName(); // Rowan Nikolaus
+ let randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+ let randomCard = faker.helpers.createCard(); // random contact card containing many properties
+
+ let person = {
+   name:randomName,
+   email:randomEmail,
+   card: randomCard
+ }
+ return person;
+}
+
+
+module.exports = router;
